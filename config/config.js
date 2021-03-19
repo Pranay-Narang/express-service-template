ab29aacab0ab75d85bfd6a6b61c0e743f73eeb11
Pranay Narang <pranaynarang@gmail.com>
2021-03-19 19:21:28 +0530

    Add firebase-admin support

--- config/config.js
+++ config/config.js
@@ -3,8 +3,20 @@ require('dotenv').config()
 var CONFIG = {}
 
 CONFIG.port = process.env.PORT || 3000
+CONFIG.APP_ENV = process.env.NODE_ENV || 'development'
+
 CONFIG.DB_URL = process.env.DB_URL || '127.0.0.1'
 CONFIG.DB_NAME = process.env.DB_NAME || 'template'
-CONFIG.APP_ENV = process.env.NODE_ENV || 'development'
+
+CONFIG.FIREBASE_ACCOUNT_TYPE = process.env.FIREBASE_ACCOUNT_TYPE || 'service_account'
+CONFIG.FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID
+CONFIG.FIREBASE_PRIVATE_KEY_ID = process.env.FIREBASE_PRIVATE_KEY_ID
+CONFIG.FIREBASE_PRIVATE_KEY = process.env.FIREBASE_PRIVATE_KEY
+CONFIG.FIREBASE_CLIENT_EMAIL = process.env.FIREBASE_CLIENT_EMAIL
+CONFIG.FIREBASE_CLIENT_ID = process.env.FIREBASE_CLIENT_ID
+CONFIG.FIREBASE_AUTH_URI = process.env.FIREBASE_AUTH_URI || 'https://accounts.google.com/o/oauth2/auth'
+CONFIG.FIREBASE_TOKEN_URI = process.env.FIREBASE_TOKEN_URI || 'https://accounts.google.com/o/oauth2/auth'
+CONFIG.FIREBASE_AUTH_PROVIDER_x509_CERT_URL = process.env.FIREBASE_AUTH_PROVIDER_x509_CERT_URL || 'https://www.googleapis.com/oauth2/v1/certs'
+CONFIG.FIREBASE_CLIENT_x509_CERT_URL = process.env.FIREBASE_CLIENT_x509_CERT_URL
 
 module.exports = CONFIG
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('morgan')
+const admin = require("firebase-admin");
 
 const CONFIG = require('./config/config')
 const routes = require('./routes')
+
+const firebaseServiceAccount = {
+    type: CONFIG.FIREBASE_ACCOUNT_TYPE,
+    project_id: CONFIG.FIREBASE_PROJECT_ID,
+    private_key_id: CONFIG.FIREBASE_PRIVATE_KEY_ID,
+    private_key: CONFIG.FIREBASE_PRIVATE_KEY,
+    client_email: CONFIG.FIREBASE_CLIENT_EMAIL,
+    client_id: CONFIG.FIREBASE_CLIENT_ID,
+    auth_uri: CONFIG.FIREBASE_AUTH_URI,
+    token_uri: CONFIG.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: CONFIG.FIREBASE_AUTH_PROVIDER_x509_CERT_URL,
+    client_x509_cert_url: CONFIG.FIREBASE_CLIENT_x509_CERT_URL
+}
 
 // TODO: Add an error handler for connection failure
 mongoose.connect('mongodb://' + CONFIG.DB_URL + '/' + CONFIG.DB_NAME, {
@@ -12,6 +26,10 @@ mongoose.connect('mongodb://' + CONFIG.DB_URL + '/' + CONFIG.DB_NAME, {
     useFindAndModify: true,
     useCreateIndex: true
 })
+
+admin.initializeApp({
+    credential: admin.credential.cert(firebaseServiceAccount)
+});
 
 const app = express()
 app.use(express.json())
